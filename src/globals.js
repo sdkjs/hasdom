b3ac22ff39665f6497865fe14b6ba85653676d4f
高梵梵高 <xugaofan@gmail.com>
2015-06-29 16:59:10 +0800

    Update globals.js
    get global in strict mode

--- src/globals.js
+++ src/globals.js
@@ -1,9 +1,14 @@
 /*jshint -W079*/
-var root = function() {
-	return this
-}
 
-var global = root() || {}
+/**
+ * 严格模式下this为undefined
+ * see http://stackoverflow.com/questions/9642491/getting-a-reference-to-the-global-object-in-an-unknown-environment-in-strict-mod
+ */
+
+/*jshint -W067*/
+var global = (1, eval)('this');
+/*jshint +W067*/
+
 var document = global.document
 
 export {global, document}
